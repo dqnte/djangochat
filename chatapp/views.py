d577c85428d9269f45fec63ee5197cdad397ec95
Dante Tobar <dante.m.tobar@gmail.com>
2020-03-17 14:13:55 -0700

    index deliverer

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# chatapp/views.py
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'chatapp/index.html')
